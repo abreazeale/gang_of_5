545fa4c464d5f2a437aa89600fdb358555bcd7be
pinargultekin <40220618+pinargultekin@users.noreply.github.com>
2018-11-20 19:48:52 -0500

    Update oshopping.js

--- oshopping.js
+++ oshopping.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
           el.parent().parent().remove(); 
           if($(".product").length == 0) {
             if(check) {
-              $("#cart").html("<h1>The shop does not function, yet!</h1><p>If you liked my shopping cart, please take a second and heart this Pen on <a href='https://codepen.io/ziga-miklic/pen/xhpob'>CodePen</a>. Thank you!</p>");
+              $("#cart").html("<h1>The shop does not function, yet!</h1><p></a>. Thank you!</p>");
             } else {
               $("#cart").html("<h1>No products!</h1>");
             }
